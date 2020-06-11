c4f9d539fcba4b1ad6859e72d88d6ee3a14e6eff
Albert Felix <59144143+albert-felix@users.noreply.github.com>
2020-06-11 08:16:03 +0530

    Update userProfile.js

--- src/pages/userProfile.js
+++ src/pages/userProfile.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 
 const userProfile = () => {
+  
   const currentUser = window.localStorage.getItem("email");
   const data = { currentUser };
 
